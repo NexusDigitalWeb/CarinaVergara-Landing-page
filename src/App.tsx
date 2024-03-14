6d8ee617fa0fd5aabd5a53dc90bdb6a2d9431053
Lucio Gastellu Arrieta <104010048+lucio1907@users.noreply.github.com>
2024-03-14 11:16:46 -0300

    configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import './index.css'
+import React from "react";
+import "./index.css";
 
 const App = (): React.ReactElement => {
   return (
     <div>
-      <h1 className='carina-gradient-color text-[70px] font-bold'>Primer Proyecto Oficial</h1>
+      <h1 className="carina-gradient-color text-[70px] font-bold">
+        Primer Proyecto Oficial
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
