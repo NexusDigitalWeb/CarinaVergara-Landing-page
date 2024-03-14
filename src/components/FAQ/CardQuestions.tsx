551e88fb660a7dad06af41e52917b5be6cf33d86
Tomas <tomascerdeyraimport@gmail.com>
2024-03-14 16:38:43 -0300

    CarsQeustions

--- src/components/FAQ/CardQuestions.tsx
+++ src/components/FAQ/CardQuestions.tsx
@@ -6,11 +6,17 @@ const CardQuestions: React.FC<CardQuestion> = ({ pregunta, respuesta }) => {
     return (
         <div >
             <details>
-                <summary className='list-none flex items-center justify-between gap-1 bg-[#F4F4F4] w-[270px] h-[60px] text-[15px] border-2  '>
+                <summary
+                    className='list-none flex items-center justify-between gap-1 
+                    bg-[#F4F4F4] min-w-[270px] h-[70px] p-2 text-[15px] text shadow font-bold  '>
                     {pregunta}
                     <img src={Flecha} alt="" />
                 </summary>
-                <p className='bg-[#03A7AD] rounded-bl-lg rounded-br-lg border-b-2 text-center text-[15px] '>{respuesta}</p>
+                <p
+                    className='bg-[#03A7AD] rounded-bl-2xl rounded-br-2xl 
+                    border-b-2 text-center text-[14px] text-white p-2 '>
+                    {respuesta}
+                </p>
             </details>
         </div>
     )
