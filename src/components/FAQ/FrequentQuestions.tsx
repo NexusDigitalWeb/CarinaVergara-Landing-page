81026e86ad7e339458209482f261b7d7e3d5637a
Tomas <tomascerdeyraimport@gmail.com>
2024-03-15 13:27:20 -0300

    feat: Footer desktop completed

--- src/components/FAQ/FrequentQuestions.tsx
+++ src/components/FAQ/FrequentQuestions.tsx
@@ -5,7 +5,7 @@ import CardQuestions from './CardQuestions';
 
 const FrequentQuestions = (): React.ReactElement => {
   return (
-    <div className='px-4 mb-20 w-full'>
+    <div className='px-4 md:px-16 mb-20 w-full'>
       <h2 className='text-3xl mb-6'>Preguntas Frecuentes</h2>
       <div className='flex flex-col gap-6'>
         {questions.preguntasFrecuentes.map((item: Questions, index: number) => (
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import LogoCarina from '../../../public/LogoCarina.svg';
 import FooterImage from '../../../public/FooterImage.svg';
 import menu from '../../json/menu.json';
@@ -10,24 +11,67 @@ import InstagramIcon from '../../../public/SocialInstagram.svg';
 import CopyrighIcon from '../../../public/CopyrighIcon.svg';
 
 const Footer = (): React.ReactElement => {
+
+  const [modal, setModal] = useState(false);
+
+  useEffect(() => {
+
+    const screenSize = () => {
+      setModal(window.innerWidth >= 1024)
+    }
+
+    screenSize();
+
+    //Detecto cambios 
+    window.addEventListener('resize', screenSize);
+
+    //Limpio el evento 
+    return () => {
+      window.removeEventListener('resize', screenSize)
+    }
+
+  }, [])
+
+
   return (
-    <footer className='relative bg-water-green text-white p-2 w-full -z-10'>
-      <div className='flex flex-col items-center justify-center'>
-        <div className='z-10 flex flex-col items-center'>
-          <img src={LogoCarina} width='150' height='200' alt='' />
-        </div>
-        <div className='absolute bottom-2 w-full h-[450px] max-w-[450px] z-[1]'>
-          <img className='w-full h-full ' src={FooterImage} alt='' />
-        </div>
-      </div>
-      <div className='z-10 flex flex-col gap-2 mb-5 min-w-[150px] justify-center items-center'>
-        {menu.menu.map((item: MenuItenrface, index: number) => (
-          <div key={index} className='border-b text-center p-5 w-[200px] z-[1]'>
-            <NavLink to={item.to}>{item.text}</NavLink>
+    <footer className='relative gap-2 bg-water-green text-white p-2 lg:px-16 lg:py-5 w-full -z-10 flex flex-col lg:justify-between'>
+      <div>
+        <div className='flex flex-col items-center justify-center lg:items-end'>
+          <div className='z-10 flex flex-col items-center lg:mr-24'>
+            <img src={LogoCarina} width='150' height='200' alt='' />
           </div>
-        ))}
+          <div className='absolute bottom-2 lg:bottom-10 right-23 w-full h-[450px] max-w-[350px] z-[1]'>
+            <img className='w-full h-full lg:h-[400px]' src={FooterImage} alt='' />
+          </div>
+        </div>
+        <div className='z-10 flex flex-col gap-2 mb-5 min-w-[150px] justify-center items-center lg:items-start lg:gap-20 lg:justify-start lg:h-full'>
+          {
+            modal && (
+              <>
+                <div className='flex flex-col gap-2 w-[400px] pl-8'>
+                  <p className='text-2xl'>Carina Vergara</p>
+                  <p className=' font-thin'>
+                    coach deportiva
+                  </p>
+                </div>
+                <div className='flex flex-col gap-2 w-[400px] pl-8'>
+                  <p className='text-2xl'>Contacto</p>
+                  <p className=' font-thin'>vergaracarina78@gmail.com</p>
+                  <p className=' font-thin'>+54 11 6281-0278</p>
+                </div>
+              </>
+            )
+          }
+          <div className='flex flex-col lg:flex-row'>
+            {menu.menu.map((item: MenuItenrface, index: number) => (
+              <div key={index} className='border-b text-center p-5 w-[200px] z-[1] lg:w-[105px] lg:border-b-0 lg:border-r lg:text-sm'>
+                <NavLink to={item.to}>{item.text}</NavLink>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-      <div className='z-10 flex flex-col gap-5'>
+      <div className='z-10 flex flex-col gap-5 lg:flex-row lg:items-center lg:justify-between lg:border-t lg:px-8 lg:pt-5'>
         <div className='flex items-center justify-center gap-2 z-[1]'>
           <Link to='' target='_blank'>
             <img src={InstagramIcon} alt='instagram-logo' />
