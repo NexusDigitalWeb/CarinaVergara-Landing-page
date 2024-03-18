1ec71452b2cb65c61d4e29c855c0117bbcca5f69
Tomas <tomascerdeyraimport@gmail.com>
2024-03-18 16:02:53 -0300

    feat: Abouteme responsive completed

--- src/components/aboutme/AboutMe.tsx
+++ src/components/aboutme/AboutMe.tsx
@@ -1,27 +1,34 @@
 import React from "react";
-import ContactMe from "../buttons/ContactMe";
+import ContactMe from "../buttons/ContactMe"
+import CarinaImg from "../../../public/CarinaVergara.jpg"
 
 const AboutMe = (): React.ReactElement => {
   return (
-    <div className="h-full w-full p-4">
-      <h2 className="w-[233px] h-[96px] text-3xl">Acerca de mí</h2>
-      <div className="bg-gradient rounded-xl min-w-[270px] h-[654px] shadow-xl flex flex-col items-center justify-center gap-8">
-        <div>
-          <img
-            className=" bg-black rounded-xl  min-w-[260px] h-[293px]"
-            src=""
-            alt=""
-          />
+    <div className="h-full w-full px-4 md:px-20 flex flex-col gap-10">
+      <h2 className="text-3xl lg:text-[55px]">Acerca de mí</h2>
+      <div className="flex justify-center">
+        <div className="bg-gradient rounded-xl min-w-[270px] max-w-[1030px] h-[654px] shadow-xl 
+      flex flex-col items-center justify-center lg:flex-row gap-8 lg:px-10"
+        >
+          <div>
+            <img
+              className=" bg-black rounded-xl md:w-[300] md:h-[320px] lg:h-[500px] min-w-[260px] h-[293px]"
+              src={CarinaImg}
+              alt=""
+            />
+          </div>
+          <div className="flex flex-col items-center justify-center gap-5 lg:h-[500px] lg:justify-start lg:gap-28  lg:w-[435px]">
+            <h3 className="text-2xl font-[500 lg:text-4xl">Coach Deportiva</h3>
+            <p className="text-center px-4 md:px-20 lg:px-0 lg:text-xl">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. fdsf
+              sdfsdfe sfegwqwegwg Culpa sed eligendi officia! Velit facilis
+              corporis accusamus Lorem, ipsum dolor sit amet consectetur adipisicing elit. fdsf,
+            </p>
+          </div>
         </div>
-        <div className="flex flex-col items-center justify-center gap-5">
-          <h3 className="text-2xl font-[500]">Coach Deportiva</h3>
-          <p className=" text-center px-4">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. fdsf
-            sdfsdfe sfegwqwegwg Culpa sed eligendi officia! Velit facilis
-            corporis accusamus,
-          </p>
-        </div>
+      </div>
 
+      <div className="flex items-center justify-center">
         <ContactMe text="Contactame!" link="sdfsfsf" />
       </div>
     </div>
