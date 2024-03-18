a947a6fb018249c5f06bf59d6346bc5eace068de
Tomas <tomascerdeyraimport@gmail.com>
2024-03-18 11:34:18 -0300

    feat: FAQ responsive completed

--- src/components/FAQ/CardQuestions.tsx
+++ src/components/FAQ/CardQuestions.tsx
@@ -2,18 +2,18 @@ import React from 'react'
 import { CardQuestion } from '../../types/interfaces'
 import Flecha from '../../../public/Flecha.svg'
 
-const CardQuestions: React.FC<CardQuestion> = ({ pregunta, respuesta }) => {
+const CardQuestions: React.FC<CardQuestion>  = ({ pregunta, respuesta }) => {
     return (
         <details>
             <summary
                 className='list-none flex items-center justify-between gap-1 
-                    bg-[#F4F4F4] min-w-[270px] h-[70px] p-2 text-[15px] text shadow font-bold  '>
+                    bg-[#F4F4F4] lg:bg-transparent min-w-[270px] h-[70px] lg:h-[100px] p-2 text-[15px] md:text-[16px] lg:text-[20px] text-[#220953] shadow font-bold lg:font-normal  '>
                 {pregunta}
                 <img src={Flecha} alt="" />
             </summary>
             <p
                 className='bg-[#03A7AD] rounded-bl-2xl rounded-br-2xl 
-                    border-b-2 text-center text-[14px] text-white p-2 '>
+                    border-b-2 text-center text-[14px] md:text-[15px] lg:text-[16px] text-white p-2 '>
                 {respuesta}
             </p>
         </details>
--- src/components/FAQ/FrequentQuestions.tsx
+++ src/components/FAQ/FrequentQuestions.tsx
@@ -2,19 +2,24 @@ import React from "react";
 import questions from "../../json/questions.json";
 import { Questions } from "../../types/interfaces";
 import CardQuestions from "./CardQuestions";
+import ContactMe from "../buttons/ContactMe";
 
 const FrequentQuestions = (): React.ReactElement => {
   return (
-    <div className="mb-20 p-4">
-      <h2 className="text-3xl mb-6">Preguntas Frecuentes</h2>
-      <div className="flex flex-col gap-6">
+    <div className="mb-20 p-4 flex flex-col gap-8 lg:gap-24">
+      <h2 className="text-3xl lg:text-[55px] mb-6">Preguntas Frecuentes</h2>
+      <div className="flex flex-col lg:flex-row lg:flex-wrap lg:justify-center gap-10 ">
         {questions.preguntasFrecuentes.map((item: Questions, index: number) => (
-          <CardQuestions
-            key={index}
-            pregunta={item.pregunta}
-            respuesta={item.respuesta}
-          />
+          <div key={index} className="lg:w-1/3">
+            <CardQuestions
+              pregunta={item.pregunta}
+              respuesta={item.respuesta}
+            />
+          </div>
         ))}
+      </div>
+      <div className="flex items-center justify-center">
+        <ContactMe text="Contactame!" link="sdfsfsf" />
       </div>
     </div>
   );
